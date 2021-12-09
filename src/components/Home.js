2f9a2d43cd682d98da214805fde8cf2d14bfbbc5
Fastidio, Althea (af0399) <af0399@att.com>
2021-12-09 12:52:58 -0800

    Home page cleanup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,45 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import Tab from './Tab';
+import QuestionCard from './QuestionCard';
 
-const Home = ({ unanswered, answered }) => {
-    const [selectedTab, setSelectedTab] = useState(1);
-
-    const showTab = (tabId, tabLabel) => {
-        return (
-            <li
-                className={`${selectedTab === tabId ? 'bg-blue-400 text-white' : 'bg-white text-blue-400'} w-1/2 text-center rounded-l-lg cursor-pointer`}
-                onClick={() => setSelectedTab(tabId)}
-            >
-                {tabLabel}
-            </li>
-        )
-    }
+const Home = ({ dispatch, ...props }) => {
+    const [selectedTab, setSelectedTab] = useState('unanswered');
 
     return (
         <div className="flex flex-col items-center w-full">
             <ul className="flex flex-row justify-evenly border border-blue-400 rounded-lg h-8 w-1/2">
-                {showTab(1, "Unanswered")}
-                {showTab(2, "Answered")}
+                {
+                    Object.keys(props).map(tab => (
+                        <li
+                            className={`${selectedTab === tab ? 'bg-blue-400 text-white' : 'bg-white text-blue-400'} w-1/2 text-center rounded-lg capitalize cursor-pointer`}
+                            onClick={() => setSelectedTab(tab)}
+                        >
+                            {tab}
+                        </li>
+                    ))
+                }
             </ul>
             <ul className="w-1/3">
-                {selectedTab === 1 ? <Tab questions={unanswered} /> : <Tab questions={answered} />}
+                {
+                    props[selectedTab].map(id => (
+                        <li key={id}>
+                            <QuestionCard id={id} />
+                        </li>
+                    ))
+                }
             </ul>
         </div>
     )
-
 }
 
 function mapStateToProps({ questions, users, currentUser }) {
     return {
         unanswered: Object.keys(questions)
-                    .filter(id => !Object.keys(users[currentUser]['answers']).includes(id))
-                    .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
+            .filter(id => !Object.keys(users[currentUser]['answers']).includes(id))
+            .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
         answered: Object.keys(questions)
-                    .filter(id => Object.keys(users[currentUser]['answers']).includes(id))
-                    .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
+            .filter(id => Object.keys(users[currentUser]['answers']).includes(id))
+            .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
     }
 }
 
